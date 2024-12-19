e422e75d12966f057962c5a012334c5cd89e5821
Vitor Portes <vitorportnoy@gmail.com>
2024-12-19 14:01:08 -0300

    adds status endpoint proper response

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,22 @@
 import database from "infra/database.js";
+import { userAgent } from "next/server";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows[0]);
+  const updatedAt = new Date();
+  const result = await database.query(`
+  SELECT 
+      version() AS postgres_version,
+      (SELECT setting FROM pg_settings WHERE name = 'max_connections') AS max_connections,
+      (SELECT COUNT(*) FROM pg_stat_activity) AS used_connections
+  `);
+  const { postgres_version, max_connections, used_connections } =
+    result.rows[0];
+
   response.status(200).json({
-    status: 200,
-    message: "API working",
+    updated_at: updatedAt,
+    postgres_version: postgres_version,
+    max_connections: max_connections,
+    used_connections: used_connections,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("GET to /api/v1/status should return status 200", async () => {
-  const result = await fetch("http://localhost:3000/api/v1/status");
-  expect(result.status).toBe(200);
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.postgres_version).toBeDefined();
+  expect(responseBody.max_connections).toBeDefined();
+  expect(responseBody.used_connections).toBeDefined();
 });
